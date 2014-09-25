55e38eac483225213ce2ca4472dded4c6909cd97
Artem Demo <artemdemo@gmail.com>
2014-09-25 22:51:43 +0200

    *

--- js/app.js
+++ js/app.js
@@ -5,9 +5,13 @@ var weather = angular.module('weather', [])
 	$scope.menuOpen = '';
 
 	$scope.menuToggle = function() {
-
 		$scope.menuOpen = ! $scope.menuOpen ? 'st-menu-open' : '';
-
 	}
+
+	$scope.openWeatherPopUp = function () {
+		$scope.weatherPopUpOpen = ! $scope.weatherPopUpOpen ? true : false;
+	}
+
+	
 
 })
