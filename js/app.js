5a47b1be4c61a691da3600e23a83adbcfe9683a4
Artem Demo <artemdemo@gmail.com>
2014-09-26 09:56:06 +0200

    Daily forecast for 10 days

--- js/app.js
+++ js/app.js
@@ -12,14 +12,30 @@ var weather = angular.module('weather', [])
 		$scope.weatherPopUpOpen = ! $scope.weatherPopUpOpen ? true : false;
 	}
 
-/*
-	$http({method: 'GET', url: 'http://api.openweathermap.org/data/2.5/forecast?q=moscow,ru&units=metric&lang=ru'}).
+
+	//$http({method: 'GET', url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Moscow&units=metric&cnt=10&lang=ru'}).
+	$http({method: 'GET', url: 'moscow.json'}).
 		success(function(data, status, headers, config) {
 			console.log( data );
+			$scope.weatherObject = data;
+			$scope.forecast = data.list;
 		}).
 		error(function(data, status, headers, config) {
-		// called asynchronously if an error occurs
-		// or server returns response with an error status.
+			console.log( data );
+			console.log( status );
 		});
-*/
+
+	$scope.convertDate = function(unix_stamp){
+		var a = new Date(unix_stamp*1000);
+		var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+		var month = months[a.getMonth() - 1];
+		var date = a.getDate();
+		var time = date + ' ' + month;
+		return time;
+	}
+
+	$scope.getWeatherIcon = function( icon ) {
+		return 'http://openweathermap.org/img/w/'+ icon +'.png'
+	}
+
 })
